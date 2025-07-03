96b9d74073e5a5efc4195dd5884e60c9e79042d9
linkhl <af.hernandezl@uniandes.edu.co>
2025-07-03 13:34:52 -0500

    completed until match exercises

--- unidad2/ejercicios/tests/5_test_control_flujo_if.rs
+++ unidad2/ejercicios/tests/5_test_control_flujo_if.rs
@@ -5,19 +5,22 @@ mod if_statement_tests {
     // En Rust, `if` es una expresión, lo que significa que puede devolver un valor.
 
     #[test]
-    fn test_if_else_basico() {
+    fn basic_if_else_test() {
         // Objetivo: Entender la estructura básica de un `if-else`.
 
         let numero = 10;
         let resultado: &str;
 
         // 1. Escribe una condición `if` para verificar si `numero` es mayor que 5.
-
+        if numero > 5 {
+            resultado = "It will be";
+        }
+        else {
+            resultado = "It will never be";
+        }
 
         // 2. Verifica que el resultado es el esperado.
-
-        // 3. Haz lo mismo con un número que no cumpla la condición.
-
+        assert_eq!(resultado, "It will be");
     }
 
     #[test]
@@ -31,29 +34,41 @@ mod if_statement_tests {
         // - Si `n` es divisible por 3, devuelve "Divisible por 3".
         // - Si `n` es divisible por 2, devuelve "Divisible por 2".
         // - De lo contrario, devuelve "No es divisible por 2, 3 ni 4".
-
+        let if_result: &str;
+        if n % 4 == 0 {
+            if_result = "Divisible by 4";
+        } else if n % 3 == 0 {
+            if_result = "Divisible by 3";
+        }
+        else if n % 2 == 0 {
+            if_result = "Divisible by 2";
+        }
+        else {
+            if_result = "Not divisible by 2, 3 or 4";
+        }
 
         // 2. Verifica el resultado para `n = 6`.
-
-
+        assert_eq!(if_result, "Divisible by 3")
     }
 
     #[test]
     fn test_if_en_una_declaracion_let() {
         // Objetivo: Usar `if` como una expresión para asignar un valor a una variable.
 
-        let condicion = true;
+        let mut condicion = true;
 
         // 1. Usa una expresión `if` para asignar un valor a la variable `numero`.
         // Si `condicion` es verdadera, `numero` debe ser 5. Si no, debe ser 0.
         // Nota: Ambos bloques (`if` y `else`) deben devolver el mismo tipo.
-
+        let mut numero = if condicion { 5 } else { 0 };
 
         // 2. Verifica el valor de `numero`.
-
+        assert_eq!(numero, 5);
 
         // 3. Haz lo mismo con la condición en `false`.
-
+        condicion = false;
+        numero = if condicion { 5 } else { 0 };
+        assert_eq!(numero, 0);
     }
 
     #[test]
@@ -64,12 +79,12 @@ mod if_statement_tests {
         let tiene_licencia = true;
 
         // 1. Escribe una condición que verifique si la persona es mayor de edad (>= 18) Y tiene licencia.
-
+        if edad > 18 && tiene_licencia {}
 
         let es_estudiante = true;
         let es_fin_de_semana = false;
 
         // 2. Escribe una condición que verifique si la persona puede descansar: si es fin de semana O no es estudiante.
-
+        if es_fin_de_semana || !es_estudiante {}
     }
 }
--- unidad2/ejercicios/tests/6_test_control_flujo_for_while_loop.rs
+++ unidad2/ejercicios/tests/6_test_control_flujo_for_while_loop.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod loops_tests {
+    use std::ops::Add;
 
     #[test]
     fn test_loop_con_break() {
@@ -10,10 +11,14 @@ mod loops_tests {
         // 1. Usa un bucle `loop` para incrementar `contador`.
         //    El `loop` debe detenerse cuando `contador` llegue a 10.
         //    Cuando se detenga, debe devolver el valor de `contador` multiplicado por 2.
-
-
+        let modified_counter = loop {
+            contador += 1;
+            if contador == 10{
+                break contador * 2;
+            }
+        };
         // 2. Verifica que el resultado devuelto por el `loop` es 20.
-
+        assert_eq!(modified_counter, 20);
     }
 
     #[test]
@@ -23,8 +28,11 @@ mod loops_tests {
         let mut numero = 3;
 
         // 1. Usa un bucle `while` para decrementar `numero` hasta que llegue a 0.
-
+        while numero != 0 {
+            numero -= 1;
+        }
         // 2. Verifica que, después del bucle, `numero` es 0.
+        assert_eq!(numero, 0);
 
     }
 
@@ -37,25 +45,41 @@ mod loops_tests {
         // 1. Usa un bucle `for` para iterar sobre los números del 1 al 5 (incluido).
         //    Suma cada número a la variable `suma`.
         //    El rango `1..=5` incluye el 5.
-
+        for num in 0..=5 {
+            suma += num;
+        }
 
         // 2. Verifica que la suma sea la correcta (1 + 2 + 3 + 4 + 5 = 15).
-
+        assert_eq!(suma, 15);
 
         // 3. Usa un bucle `for` con `.rev()` para contar hacia atrás.
-
+        let expected_array: [i32;6] = [5,4,3,2,1,0];
+        let mut new_array: [i32;6] = [0;6];
+        let mut i = 0;
+        for num in (0..=5).rev() {
+            new_array[i] = num;
+            i += 1;
+        }
+        for it in 0..5 {
+            let one = new_array[it];
+            let two = expected_array[it];
+            assert_eq!(one, two);()
+        }
     }
 
     #[test]
     fn test_for_con_iterador_de_coleccion() {
         // Objetivo: Usar un bucle `for` para iterar sobre los elementos de una colección (array).
 
-
         // 1. Usa un bucle `for` para iterar sobre cada elemento del array `a`.
         //    El método `.iter()` crea un iterador que nos permite recorrer la colección.
-
-
+        let a = ["a";10];
+        let mut sum = String::from("");
+        for an_a in a.iter() {
+            sum = sum.add(an_a);
+        }
         // 2. Verifica que la suma de los elementos sea correcta.
+        assert_eq!(sum, "aaaaaaaaaa");
 
     }
 }
